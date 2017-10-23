8fd7550b683ad8535b14fef8347a798f9d4684be
Saravanabalagi Ramachandran <s1753338@ivanisevic.inf.ed.ac.uk>
2017-10-23 22:20:08 +0100

    Added autonomous movement, added find_satellite

---
+++ find_satellite.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+SATELLITE_X = -0.69
+SATELLITE_Y = 0
+SATELLITE_Z = 2.95
+
+def find_satellite(x, y, theta):
+
+        # finding required bot rotation
+        slope_y = (y - SATELLITE_Y) / (x - SATELLITE_X)
+        required_bot_rotation = np.rad2deg(np.arctan(slope_y)) - theta + 180
+
+        # optimize angle
+        if abs(required_bot_rotation) > 180:
+            if required_bot_rotation > 0: required_bot_rotation -= 360
+            else: required_bot_rotation += 360
+
+        # finding required servo angle
+        distance_from_satellite = np.sqrt((y - SATELLITE_Y)**2 + (x - SATELLITE_X)**2)
+        slope_z = SATELLITE_Z / distance_from_satellite
+        required_servo_angle = np.rad2deg(np.arctan(slope_z))
+
+        return required_bot_rotation, required_servo_angle
+        
+print(find_satellite(1.5, 1.5, 45))
+print("169.40847225202862, 48.018699563443406")
--- toddler.py
+++ toddler.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 __TODDLER_VERSION__ = "1.0.0"
 
+# constants
+THRESHOLD_SONAR = 19
+THRESHOLD_LIGHT = 100
+THRESHOLD_IR = 200
+
+SATELLITE_X = 0.69
+SATELLITE_Y = 0.00
+SATELLITE_Z = 2.95
+
 import time
-import numpy
+import numpy as np
 import cv2
 import datetime
 
@@ -11,70 +20,124 @@ class Toddler:
     def __init__(self, IO):
         print 'I am a toddler playing in a sandbox'
         self.IO = IO
-        self.inp = [0, 0, 0, 0, 0, 0, 0, 0]
+        self.digital = [0, 0, 0, 0, 0, 0, 0, 0]
+        self.analog = [0, 0, 0, 0, 0, 0, 0, 0]
+        self.emergency_stop = False
 
-    # I just added a comment. Wow its working :)
-    def move(self, l, r):
+    # Move where space is available
+    def move(self):
+
+        ir_left = self.analog[1]
+        ir_right = self.analog[2]
+
+        l = ir_left <= THRESHOLD_IR
+        r = ir_right <= THRESHOLD_IR
+
+        # if both left and right are blocked
         if not l and not r:
-            return [0, 0]
+            self.IO.setMotors(-100, -100)
+            time.sleep(1)
+
+        # if left is free but right is blocked
         if l and not r:
-            return [100, -100]
+            self.IO.setMotors(100, -100)
+            time.sleep(0.5)
+
+        # if right is free but left is blocked
         if not l and r:
-            return [-100, 100]
+            self.IO.setMotors(-100, 100)
+            time.sleep(0.5)
+
+        # if both left and right are available
         if l and r:
-            return [100, 100]
+            self.IO.setMotors(100, -100)
 
-            # This is a callback that will be called repeatedly.
+    def rotate(self, degrees):
+        if degrees == 0: return
+        if degrees >= 0: self.IO.setMotors(-100, 100)
+        else: self.IO.setMotors(100, -100)
+        time.sleep(3)
+        return
 
+    # x and y distance from deployment base
+    # theta is angle from deployment base, 0 pointing east
+    def find_satellite(self, x, y, theta):
+
+        # finding required bot rotation
+        slope_y = (y - SATELLITE_Y) / (x - SATELLITE_X)
+        required_bot_rotation = np.rad2deg(np.arctan(slope_y)) - theta + 180
+
+        # optimize angle
+        if abs(required_bot_rotation) > 180:
+            if required_bot_rotation > 0: required_bot_rotation -= 360
+            else: required_bot_rotation += 360
+
+        # finding required servo angle
+        distance_from_satellite = np.sqrt((y - SATELLITE_Y)**2 + (x - SATELLITE_X)**2)
+        slope_z = SATELLITE_Z / distance_from_satellite
+        required_servo_angle = np.rad2deg(np.arctan(slope_z))
+
+        return required_bot_rotation, required_servo_angle
+
+    # This is a callback that will be called repeatedly.
     # It has its dedicated thread so you can keep block it.
     def Control(self, OK):
-        mot = [False, False, False]
-        motPrev = [False, False, False]
-        motSwitch = 0
 
-        pos = 180
+        # mot = [False, False, False]
+        # motPrev = [False, False, False]
+        # pos = 180
+
         while OK():
+
+            # Read once in every 0.05s
             time.sleep(0.05)
-            self.inp = self.IO.getInputs()
-            analog = self.IO.getSensors()
-            # print(analog)
-            # mot[0] = self.inp[1]
-            # mot[1] = self.inp[2]
-            # mot[2] = self.inp[3]
 
-            print(analog, self.inp)
+            # Read Sensor Data
+            self.digital = self.IO.getInputs()
+            self.analog = self.IO.getSensors()
 
-            if self.inp[0] or self.inp[1]:
+            # Print sensor data to console
+            print(self.analog, self.digital)
+
+            # Emergency stop on mot
+            if self.digital[6]: self.emergency_stop = not self.emergency_stop
+            if self.emergency_stop: self.IO.setMotors(0, 0)
+
+            # Stop on POI
+            elif self.analog[3] > THRESHOLD_LIGHT:
                 self.IO.setMotors(0, 0)
-            elif (analog[3] > 200):
-                self.IO.setMotors(0, 0)
-            elif (analog[0] > 100):
-                self.IO.setMotors(-80, -100)
+                time.sleep(10)
+
+            # turn or reverse when sonor is blocked
+            elif self.analog[0] > THRESHOLD_SONAR or self.digital[0] or self.digital[1]:
+                self.move()
+
+            # keep going forward otherwise
             else:
-                self.IO.setMotors(0, 0)
+                self.IO.setMotors(100,100)
 
-            # if mot[0] != motPrev[0] or mot[1] != motPrev[1]:
-            #     speed = self.move(mot[0], mot[1])
-            #     self.IO.setMotors(-speed[0], speed[1])
-            #     if mot[0]:
-            #         self.IO.setStatus('on')
-            #     else:
-            #         self.IO.setStatus('off')
-            #     if mot[1]:
-            #         self.IO.setError('on')
-            #     else:
-            #         self.IO.setError('off')
-            # if mot[2]:
-            #     self.IO.servoEngage()
-            #     # pos=(pos+3)%360
-            #     # self.IO.servoSet(abs(pos-180))
-            #     self.IO.servoSet(30)
-            #     print("Servo Activated...")
-            # if mot[2] != motPrev[2] and not mot[2]:
-            #     self.IO.servoDisengage()
-            # motPrev[0] = mot[0]
-            # motPrev[1] = mot[1]
-            # motPrev[2] = mot[2]
+                # if mot[0] != motPrev[0] or mot[1] != motPrev[1]:
+                #     speed = self.move(mot[0], mot[1])
+                #     self.IO.setMotors(-speed[0], speed[1])
+                #     if mot[0]:
+                #         self.IO.setStatus('on')
+                #     else:
+                #         self.IO.setStatus('off')
+                #     if mot[1]:
+                #         self.IO.setError('on')
+                #     else:
+                #         self.IO.setError('off')
+                # if mot[2]:
+                #     self.IO.servoEngage()
+                #     # pos=(pos+3)%360
+                #     # self.IO.servoSet(abs(pos-180))
+                #     self.IO.servoSet(30)
+                #     print("Servo Activated...")
+                # if mot[2] != motPrev[2] and not mot[2]:
+                #     self.IO.servoDisengage()
+                # motPrev[0] = mot[0]
+                # motPrev[1] = mot[1]
+                # motPrev[2] = mot[2]
 
     # This is a callback that will be called repeatedly.
     # It has its dedicated thread so you can keep block it.
@@ -85,11 +148,11 @@ class Toddler:
         sw = False
         swPrev = False
         while OK():
-            if self.inp[4]:
+            if self.digital[4]:
                 for i in range(0, 5):
                     self.IO.cameraGrab()
                 img = self.IO.cameraRead()
-                print("Picture taken...");
+                print("Picture taken...")
                 if img.__class__ == numpy.ndarray:
                     hasImage = True
                     cv2.imwrite('camera-' + datetime.datetime.now().isoformat() + '.png', img)
@@ -99,7 +162,7 @@ class Toddler:
             if hasImage:
                 self.IO.imshow('window', img)
 
-            sw = self.inp[5]
+            sw = self.digital[5]
             if sw != swPrev and sw:
                 res = (res + 1) % 4
                 if res == 0:
